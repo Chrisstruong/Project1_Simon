f87a5372121a89575a184c900607c4fcf9728191
chrisstruong <chrisstruong@gmail.com>
2022-11-17 13:30:02 -0500

    Hello at 1:29

---
+++ main.js
@@ -0,0 +1,214 @@
+let array = ""
+let answer = ""
+var timeOut
+let setConditionForEventListener = false
+const option = document.querySelectorAll('.option')
+let winCondition = true;
+let randomNum = Math.floor(Math.random()*4)+1
+let turn = 1
+array+= randomNum
+console.log(`Whole correct number in turn ${turn} is: ${array}`)
+setTimeout(runColorForOptions, 1000)
+console.log("Player's chocie: ")
+
+const option1 = document.querySelector('#option1')
+option1.style.cursor = 'pointer'
+option1.addEventListener('click', clickOpt1)
+function clickOpt1() {
+    answer+=1
+    // Check if player enter wrong any wrong number, start at first one
+    checkWrongCondition()
+    if (answer.length === array.length && answer === array) {
+        clearTimeout(timeOut)
+        checkGame()
+    }
+    this.style.backgroundColor = 'red'
+    option1.style.cursor = 'wait'
+    //make the program untouchable when changing color when clicking
+    option1.removeEventListener('click', clickOpt1)
+    setTimeout(changeColorWhenClickingOpt1, 300)
+}
+
+const option2 = document.querySelector('#option2')
+option2.style.cursor = 'pointer'
+option2.addEventListener('click',clickOpt2)
+function clickOpt2 () {
+    answer+=2
+    checkWrongCondition()
+    if (answer.length === array.length && answer === array) {
+        clearTimeout(timeOut)
+        checkGame()
+    }
+    this.style.backgroundColor = 'red'
+    option2.style.cursor = 'wait'
+    //make the program untouchable when changing color when clicking
+    option2.removeEventListener('click', clickOpt2)
+    setTimeout(changeColorWhenClickingOpt2, 300)
+}
+
+const option3 = document.querySelector('#option3')
+option3.style.cursor = 'pointer'
+option3.addEventListener('click', clickOpt3)
+function clickOpt3() {
+    answer+=3
+    checkWrongCondition()
+    if (answer.length === array.length && answer === array) {
+        clearTimeout(timeOut)
+        checkGame()
+    }
+    this.style.backgroundColor = 'red'
+    option3.style.cursor = 'wait'
+    //make the program untouchable when changing color when clicking
+    option3.removeEventListener('click', clickOpt3)
+    setTimeout(changeColorWhenClickingOpt3, 300)
+}
+
+const option4 = document.querySelector('#option4')
+option4.style.cursor = 'pointer'
+option4.addEventListener('click', clickOpt4)
+function clickOpt4() {
+    answer+=4
+    checkWrongCondition()
+    if (answer.length === array.length && answer === array) {
+        clearTimeout(timeOut)
+        checkGame()
+    }
+    this.style.backgroundColor = 'red'
+    option4.style.cursor = 'wait'
+    //make the program untouchable when changing color when clicking
+    option4.removeEventListener('click', clickOpt4)
+    setTimeout(changeColorWhenClickingOpt4, 300)
+}
+//4:45PM: Need to execute immediately when player the first and second wrong answer
+function checkGame () {
+        if (answer!== array ) {
+            console.log("You lost the game!")
+            console.log(`Your current answer is: ${answer}`)
+            option1.removeEventListener('click', clickOpt1)
+            option2.removeEventListener('click', clickOpt2)
+            option3.removeEventListener('click', clickOpt3)
+            option4.removeEventListener('click', clickOpt4)
+        } else {
+            console.log('You win the game!!')
+            answer = ""
+            randomNum = Math.floor(Math.random()*4)+1
+            array+=randomNum
+            //Write code to each options get colored
+            console.log(`Whole correct number in turn ${turn+1} is: ${array}`)
+            setTimeout(runColorForOptions, 1000)
+            console.log(`Playerchoice turn ${turn+1}: `)
+            turn ++
+            timeOut = setTimeout(checkGame, 100000000)
+        }
+    } 
+function changeColorWhenClickingOpt1 () {
+    option1.style.backgroundColor = 'aquamarine'
+    if (answer[answer.length-1] === array[answer.length-1]) {
+        option1.addEventListener('click', clickOpt1)
+    }
+    option1.style.cursor = 'pointer'
+}
+
+function changeColorWhenClickingOpt2 () {
+    option2.style.backgroundColor = 'aquamarine'
+    if (answer[answer.length-1] === array[answer.length-1]) {
+        option2.addEventListener('click', clickOpt2)
+    }
+    option2.style.cursor = 'pointer'
+}
+
+function changeColorWhenClickingOpt3 () {
+    option3.style.backgroundColor = 'aquamarine'
+    if (answer[answer.length-1] === array[answer.length-1]) {
+        option3.addEventListener('click', clickOpt3)
+    }
+    option3.style.cursor = 'pointer'
+}
+
+function changeColorWhenClickingOpt4 () {
+    option4.style.backgroundColor = 'aquamarine'
+    if (answer[answer.length-1] === array[answer.length-1]) {
+        option4.addEventListener('click', clickOpt4)
+    }
+    option4.style.cursor = 'pointer'
+}
+
+function checkWrongCondition () {
+    if (answer[answer.length-1] !== array[answer.length-1]) {
+        return checkGame()
+    } //Check latest element in answer with array
+}
+function delayForLoop (i){//This function is the most interesting
+    setTimeout(function (){
+        if (array[i]=== '1') {
+            option1.removeEventListener('click', clickOpt1)
+            option1.style.backgroundColor = 'pink'
+            setTimeout(function(){
+                option1.style.backgroundColor = 'aquamarine'   
+            },1000)
+
+        } else if (array[i] === '2') {
+            option2.removeEventListener('click', clickOpt2)
+            option2.style.backgroundColor = 'pink'
+            setTimeout(function(){
+                option2.style.backgroundColor = 'aquamarine'
+            },1000)
+
+        } else if (array[i] === '3') {
+            option3.removeEventListener('click', clickOpt3)
+            option3.style.backgroundColor = 'pink'
+            setTimeout(function(){
+                option3.style.backgroundColor = 'aquamarine'   
+            },1000)
+
+        } else  {
+            option4.removeEventListener('click', clickOpt4)
+            option4.style.backgroundColor = 'pink'
+            setTimeout(function(){
+                option4.style.backgroundColor = 'aquamarine'            
+            },1000)
+        }
+    }, 2000*i)
+    
+}
+function runColorForOptions () {//function to delay the for loop
+    for (i=0; i < array.length; i++) {
+        option1.removeEventListener('click', clickOpt1)
+        option2.removeEventListener('click', clickOpt2)
+        option3.removeEventListener('click', clickOpt3)
+        option4.removeEventListener('click', clickOpt4)
+        delayForLoop(i) 
+    }
+    setTimeout(function() {
+        option1.addEventListener('click', clickOpt1)
+        option2.addEventListener('click', clickOpt2)
+        option3.addEventListener('click', clickOpt3)
+        option4.addEventListener('click', clickOpt4)
+    },2000*(array.length))  
+}
+
+
+
+
+// for (i=0; i<10; i++) {
+//     task(i)
+// } 
+// function task(i) {
+//     setTimeout (function () {
+//         console.log(i)
+//     },2000*i)
+// }
+
+// var count = 0
+// var inID = setInterval(counter, 3000)
+// function counter () {
+//     console.log(++count)
+// }
+// console.log("Hello dog")
+
+// for (i=0;i<10; i++) {
+// let randomNum = Math.floor(Math.random()*4)+1
+// array+=randomNum
+// console.log(array)
+// console.log(typeof array)
+// }
---
+++ planning.md
@@ -0,0 +1,62 @@
+1) My choice of game:
+    + "Simon (warning: will require asynchronous timing)"
+
+2) Pseudocode for the overall gameplay: 
+    + Phase 1 - Big Picture:
+        . 1) Planning on HTML, CSS, JS
+        . 2) Focusing on JS most of the time for programming
+        . 3) HTML 
+        . 4) Editing the beauty of program with CSS
+
+    + Phase 2 - Refinemnet:
+        . 1) Planning on HTML, CSS, JS:
+            a) Creating HTML, CSS, JS files.
+            b) Adding DOCTYPE in HTML
+            c) link CSS and JS to HTML
+        . 2) Planning JS most of the time for programming
+            a) Create an array
+            b) Use Math.floor and Math.random to create new element and assign to array
+            c) Use control flow to keep the game going
+            d) Each new round with start at array[0] to array[i]
+            e) Use condition to player to keep the going or not
+            f) Add sound
+        . 3) HTML
+            a) Creating IDs and classes
+            b) Using DOM and event Listener in JS to interact with player
+        . 4) Editing the beauty of program with CSS
+            a) Design color of the options
+            b) Add background color
+            c) Editing font
+
+    + Phase 3 - JS Refactor of Phase Two
+        . 1) Planning on HTML, CSS, JS
+            a) Creating HTML, CSS, and JS files
+            b) Creating DOCTYPE in HTML
+            c) Link CSS, JS to HTML
+        . 2) Planning JS most of the time for programming
+            a) Create an an array to store new elements each round. Start at 0 elements when creating
+            b) Use Math.floor and Math.random to create random of 4 options
+            c) Assign it to array
+            d) Use while loop to keep the going if player enter correctly
+            e) Use for loop to display the number of options that player needs to enter
+            f) Using if conditions to check whether the player answers correctly or not
+            g) If yes, keep the condition true in while so as to keep the game going. Otherwise, stop the game and display message
+        . 3) HTML
+            a) Add IDS and classes
+            b) Adding DOM and event listener in JS
+            c) When the player click, store it into another array
+            d) Display music whenever player click
+            e) Display music when the number of options need to be clicked
+            f) Display current score (the level that the player is currently in)
+            g) Adding classes to options
+        . 4) Editing the beauty of program with CSS
+            a) Editing text fonts
+            b) Creating 4 models (triangle, square) stand for 4 options
+            c) Editing background color
+            d) Updating background color when the level is >= 10
+            e) Designing the color as well as shape for options
+            f) Creating a rectangular message when the player lose the game. Add fonts to it
+
+3)
+
+
