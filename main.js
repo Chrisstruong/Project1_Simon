5ac4dda9233ee053fab336bb1239eaf750a58190
chrisstruong <chrisstruong@gmail.com>
2022-11-20 18:32:54 -0500

    Update HTML CSS JS

--- main.js
+++ main.js
@@ -3,25 +3,24 @@ let answer = ""
 let highestLevel = 0
 var timeOut
 const text1 = document.createElement('span')
-let setConditionForEventListener = false
 const option = document.querySelectorAll('.option')
 const grabScore = document.querySelector('#score')
 const levelMessage = document.createElement('p')
-levelMessage.innerText = "Current level: 1 Highest Level achieved:"
+levelMessage.innerText = "Current level: 1"
 grabScore.append(levelMessage)
+const highestLevelMessage = document.createElement('p')
+highestLevelMessage.innerText = "Highest level achieved: "
+grabScore.append(highestLevelMessage)
 const lostMessage = document.querySelector('#lost-message')
 lostMessage.style.cursor = 'pointer'
 let randomNum = Math.floor(Math.random()*4)+1
 let turn = 1
 array+= randomNum
-console.log(`Whole correct number in turn ${turn} is: ${array}`)
-setTimeout(runColorForOptions, 1000)
-console.log("Player's chocie: ")
-
+setTimeout(runColorForOptions,2000)//run color options at the start after 2s
 
 const option1 = document.querySelector('#option1')
 option1.style.cursor = 'pointer'
-option1.addEventListener('click', clickOpt1)
+// option1.addEventListener('click', clickOpt1)
 function clickOpt1() {
     answer+=1
     // Check if player enter wrong any wrong number, start at first one
@@ -30,7 +29,7 @@ function clickOpt1() {
         clearTimeout(timeOut)
         checkGame()
     }
-    this.style.backgroundColor = 'rgb(172, 255, 47)'//xanh sang
+    this.style.backgroundColor = 'rgb(151, 255, 47)'//xanh sang
     option1.style.cursor = 'wait'
     //make the program untouchable when changing color when clicking
     option1.removeEventListener('click', clickOpt1)
@@ -40,7 +39,7 @@ function clickOpt1() {
 //sayhello
 const option2 = document.querySelector('#option2')
 option2.style.cursor = 'pointer'
-option2.addEventListener('click',clickOpt2)
+// option2.addEventListener('click',clickOpt2)
 function clickOpt2 () {
     answer+=2
     checkWrongCondition()
@@ -57,7 +56,7 @@ function clickOpt2 () {
 
 const option3 = document.querySelector('#option3')
 option3.style.cursor = 'pointer'
-option3.addEventListener('click', clickOpt3)
+// option3.addEventListener('click', clickOpt3)
 function clickOpt3() {
     answer+=3
     checkWrongCondition()
@@ -65,7 +64,7 @@ function clickOpt3() {
         clearTimeout(timeOut)
         checkGame()
     }
-    this.style.backgroundColor = 'yellow'
+    this.style.backgroundColor = 'rgb(251, 255, 13)'
     option3.style.cursor = 'wait'
     //make the program untouchable when changing color when clicking
     option3.removeEventListener('click', clickOpt3)
@@ -74,7 +73,7 @@ function clickOpt3() {
 
 const option4 = document.querySelector('#option4')
 option4.style.cursor = 'pointer'
-option4.addEventListener('click', clickOpt4)
+// option4.addEventListener('click', clickOpt4)
 function clickOpt4() {
     answer+=4
     checkWrongCondition()
@@ -91,9 +90,8 @@ function clickOpt4() {
 //4:45PM: Need to execute immediately when player the first and second wrong answer
 function checkGame () {
     if (answer!== array ) {
-        console.log("You lost the game!")
-        console.log(`Your current answer is: ${answer}`)
-        text1.innerText = `You have lost the game! `
+        text1.remove()
+        text1.innerText = `You have lost the game!      `
         lostMessage.append(text1)
         option1.removeEventListener('click', clickOpt1)
         option2.removeEventListener('click', clickOpt2)
@@ -104,44 +102,53 @@ function checkGame () {
         lostMessage.append(text2)
         
         text2.addEventListener('click', function () {
-            this.style.cursor = 'pointer'
             text1.remove()
             text2.remove()
-            console.log('')
-            levelMessage.innerText = `Current level: 1 Highest Level achieved: ${highestLevel}`
+            levelMessage.innerText = `Current level: ${1}`
             grabScore.append(levelMessage)
+            highestLevelMessage.innerText = `Highest level achieved: ${highestLevel}`
+            grabScore.append(highestLevelMessage)
             array = ""
             answer = ""
             randomNum = Math.floor(Math.random()*4)+1
             array+=randomNum
-            console.log(`Whole correct number in turn ${turn} is: ${array}`)
             setTimeout(runColorForOptions, 1000)
-            console.log("Player's chocie: ")
-            option1.addEventListener('click', clickOpt1)
-            option2.addEventListener('click', clickOpt2)
-            option3.addEventListener('click', clickOpt3)
-            option4.addEventListener('click', clickOpt4)
+            // option1.addEventListener('click', clickOpt1)
+            // option2.addEventListener('click', clickOpt2)
+            // option3.addEventListener('click', clickOpt3)
+            // option4.addEventListener('click', clickOpt4)
         })
     } else {
+        option1.removeEventListener('click', clickOpt1)
+        option2.removeEventListener('click', clickOpt2)
+        option3.removeEventListener('click', clickOpt3)
+        option4.removeEventListener('click', clickOpt4)
+        text1.remove()
         if (i>highestLevel) {
             highestLevel++
         }
-        levelMessage.innerText = `Current level: ${i+1}     Highest level achieved: ${highestLevel}`
+        levelMessage.innerText = `Current level: ${i+1}`
         grabScore.append(levelMessage)
+        highestLevelMessage.innerText = `Highest level achieved: ${highestLevel}`
+        grabScore.append(highestLevelMessage)
+        const textWin = document.createElement('span')
+        textWin.innerHTML = `Congratulations! Round ${i+1} STARTS!`
+        lostMessage.append(textWin)
+        setTimeout(function () {
+            textWin.remove()
+        },1000)
         answer = ""
         randomNum = Math.floor(Math.random()*4)+1
         array+=randomNum
         //Write code to each options get colored
-        console.log(`Whole correct number in turn ${turn+1} is: ${array}`)
         setTimeout(runColorForOptions, 1000)
-        console.log(`Playerchoice turn ${turn+1}: `)
         turn ++
         timeOut = setTimeout(checkGame, 100000000)
         }
     } 
 function changeColorWhenClickingOpt1 () {
     option1.style.backgroundColor = 'rgba(172, 255, 47, 0.594)'//xanh toi
-    if (answer[answer.length-1] === array[answer.length-1]) {
+    if (answer[answer.length-1] === array[answer.length-1] && array.length !== answer.length) {
         option1.addEventListener('click', clickOpt1)
     }
     option1.style.cursor = 'pointer'
@@ -149,7 +156,7 @@ function changeColorWhenClickingOpt1 () {
 
 function changeColorWhenClickingOpt2 () {
     option2.style.backgroundColor = 'rgba(245, 14, 14, 0.682)'
-    if (answer[answer.length-1] === array[answer.length-1]) {
+    if (answer[answer.length-1] === array[answer.length-1] && array.length !== answer.length) {
         option2.addEventListener('click', clickOpt2)
     }
     option2.style.cursor = 'pointer'
@@ -157,7 +164,7 @@ function changeColorWhenClickingOpt2 () {
 
 function changeColorWhenClickingOpt3 () {
     option3.style.backgroundColor = 'rgba(255, 255, 0, 0.652)'
-    if (answer[answer.length-1] === array[answer.length-1]) {
+    if (answer[answer.length-1] === array[answer.length-1] && array.length !== answer.length) {
         option3.addEventListener('click', clickOpt3)
     }
     option3.style.cursor = 'pointer'
@@ -165,7 +172,7 @@ function changeColorWhenClickingOpt3 () {
 
 function changeColorWhenClickingOpt4 () {
     option4.style.backgroundColor = 'rgba(0, 255, 255, 0.704)'
-    if (answer[answer.length-1] === array[answer.length-1]) {
+    if (answer[answer.length-1] === array[answer.length-1] && array.length !== answer.length) {
         option4.addEventListener('click', clickOpt4)
     }
     option4.style.cursor = 'pointer'
@@ -180,7 +187,7 @@ function delayForLoop (i){//This function is the most interesting
     setTimeout(function (){
         if (array[i]=== '1') {
             option1.removeEventListener('click', clickOpt1)
-            option1.style.backgroundColor = 'rgb(172, 255, 47)'//xanh sang
+            option1.style.backgroundColor = 'rgb(151, 255, 47)'//xanh sang
             setTimeout(function(){
                 option1.style.backgroundColor = 'rgba(172, 255, 47, 0.594)'   //xanh toi
             },1000)
@@ -194,7 +201,7 @@ function delayForLoop (i){//This function is the most interesting
 
         } else if (array[i] === '3') {
             option3.removeEventListener('click', clickOpt3)
-            option3.style.backgroundColor = 'yellow'
+            option3.style.backgroundColor = 'rgb(251, 255, 13)'
             setTimeout(function(){
                 option3.style.backgroundColor = 'rgba(255, 255, 0, 0.652)'   
             },1000)
@@ -218,36 +225,11 @@ function runColorForOptions () {//function to delay the for loop
         delayForLoop(i) 
     }
     setTimeout(function() {
+        text1.innerText = `YOUR TURN!`
+        lostMessage.append(text1)
         option1.addEventListener('click', clickOpt1)
         option2.addEventListener('click', clickOpt2)
         option3.addEventListener('click', clickOpt3)
         option4.addEventListener('click', clickOpt4)
     },2000*(array.length))  
 }
-
-
-
-
-// for (i=0; i<10; i++) {
-//     task(i)
-// } 
-// function task(i) {
-//     setTimeout (function () {
-//         console.log(i)
-//     },2000*i)
-// }
-
-// var count = 0
-// var inID = setInterval(counter, 3000)
-// function counter () {
-//     console.log(++count)
-// }
-// console.log("Hello dog")
-
-// for (i=0;i<10; i++) {
-// let randomNum = Math.floor(Math.random()*4)+1
-// array+=randomNum
-// console.log(array)
-// console.log(typeof array)
-// }
-// 
